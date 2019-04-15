34dd7ed5098320c5af172c3480a5bc5406a3fdae
Higgins, Ronan P <ronan.p.higgins@accenture.com>
2019-04-15 17:56:46 +0100

    moq commit

---
+++ Newsletter.Tests/Controllers/SubscriptionControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Newsletter.Repositories;
+
+namespace Newsletter.Tests.Controllers
+{
+    [TestClass]
+    public class SubscriptionControllerTest
+
+        //www.aamiraftab.com/unit-testing-repository-pattern-using-moq-shouldly-in-xunit-framework/
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var mock = new Mock<ISubscriptionRepository>();
+            //mock.Setup(x => x.CreateSubscription).Returns(true);
+        }
+    }
+}
